bdafa3cdbe031864314dbb5b7506ea817bbd9de5
Thomas Ries <tries@gmx.net>
2007-11-12 19:38:35 +0000

    - Small changes to compile again under OpenBSD.

--- src/dejitter.c
+++ src/dejitter.c
@@ -20,17 +20,11 @@
 */
 #include "config.h"
 
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <unistd.h>
 #include <errno.h>
-//#include <string.h>
-//#include <sys/time.h>
 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-//#include <signal.h>
-
 
 #include <osipparser2/osip_parser.h>
 #include "siproxd.h"
--- src/utils.c
+++ src/utils.c
@@ -23,7 +23,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <values.h>
+#include <limits.h>
 #include <time.h>
 #include <signal.h>
 #include <string.h>
@@ -208,8 +208,8 @@ int get_ip_by_host(char *hostname, struct in_addr *addr) {
     */
    j=0;
    k=0;
-   t1=MAXINT;
-   t2=MAXINT;
+   t1=INT_MAX;
+   t2=INT_MAX;
    for (i=0; i<DNS_CACHE_SIZE; i++) {
       if (dns_cache[i].hostname[0]=='\0') break;
       if ((dns_cache[i].expires_timestamp < t1) &&
